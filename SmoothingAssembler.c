766159a8b5d6348ee140eecf39ccb51a51ddb051
isumalatov <isumalatov@gmail.com>
2024-04-06 14:55:43 +0200

    assembler code finished

--- SmoothingAssembler.c
+++ SmoothingAssembler.c
@@ -4,7 +4,7 @@
 
 #define WIDTH 10
 #define HEIGHT 10
-#define NUM_IMAGES 1
+#define NUM_IMAGES 10
 
 typedef struct
 {
@@ -49,43 +49,57 @@ void smoothImageA(int i, int j, int width, int height, int* sumR, int* sumG, int
         loop_start:
 
         mov ebx, -1 //ebx tiene l
-        loop_start2 :
+            loop_start2 :
 
-        mov esi, ecx
-        add esi, eax
-        mov edi, edx
-        add edi, ebx
+            mov esi, ecx
+            add esi, eax
+            mov edi, edx
+            add edi, ebx
 
-        cmp esi, 0
-        jl end_if
+            cmp esi, 0
+            jl end_if
 
-        cmp esi, height
-        jge end_if
+            cmp esi, height
+            jge end_if
 
-        cmp edi, 0
-        jl end_if
+            cmp edi, 0
+            jl end_if
 
-        cmp edi, width
-        jge end_if  //esi y edi tienen ni y nj
+            cmp edi, width
+            jge end_if  //esi y edi tienen ni y nj
 
-        //aqui va lo que hay dentro del if
+            //aqui va lo que hay dentro del if
 
-        //aqui sumamos sumr, sumg, sumb
-        //aqui acaba el sumar sumr, sumg, sumb
-        
-        //aqui sumamos count
-        mov esi, count
-        add dword ptr [esi], 1 // incrementa count en 1
-        //aqui acaba el sumar count
+            //aqui va lo de sumar sumr, sumg, sumb
+            push eax
+            mov eax, img
+            mov eax, [eax + esi * 4] // obtener img[ni]
+            mov eax, [eax + edi * 4] // obtener img[ni][nj]
 
-        //aqui acaba el if
+            mov edi, sumR
+            add[edi], eax // sumar img[ni][nj].r a sumR
+            add eax, 4 // desplazarse 4 bytes para acceder a g
+            mov edi, sumG
+            add[edi], eax // sumar img[ni][nj].g a sumG
+            add eax, 4 // desplazarse 4 bytes para acceder a b
+            mov edi, sumB
+            add[edi], eax // sumar img[ni][nj].b a sumB
+            pop eax
+            //aqui sumamos sumr, sumg, sumb
+
+            //aqui sumamos count
+            mov esi, count
+            add dword ptr[esi], 1 // incrementa count en 1
+            //aqui acaba el sumar count
+
+            //aqui acaba el if
         end_if:
         inc ebx
-        cmp ebx, 1
-        jle loop_start2
-        inc eax
-        cmp eax, 1
-        jle loop_start
+            cmp ebx, 1
+            jle loop_start2
+            inc eax
+            cmp eax, 1
+            jle loop_start
     }
 }
 
